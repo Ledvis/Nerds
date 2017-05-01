7a7a2b90ce1ca69465a86ab7737c73a4d7af9fa4
Eugene <fedyai.eugen@gmail.com>
2017-05-01 10:03:50 +0300

    v 3.0 add js

---
+++ script/script.js
@@ -0,0 +1,44 @@
+var popup = document.querySelector(".modal-popup");
+var link = document.querySelector(".link");
+var close = popup.querySelector(".close");
+var overlay = document.querySelector(".overlay");
+var form = popup.querySelector("form");
+var name = form.querySelector("[name=name]");
+var email = form.querySelector("[name=email]");
+var storage = localStorage.getItem("name");
+
+link.addEventListener("click", function (event) {
+	event.preventDefault();
+	popup.classList.add("modal-popup-show");
+	overlay.classList.add("overlay-show");
+	if (storage) {
+		storage = name.value;
+		email.focus();
+	} else {
+		name.focus();
+	}
+});
+
+close.addEventListener("click", function (event) {
+	event.preventDefault();
+	popup.classList.remove("modal-popup-show");
+	overlay.classList.remove("overlay-show");
+});
+
+window.addEventListener("keydown", function (event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("modal-popup-show")) {
+			popup.classList.remove("modal-popup-show");
+			overlay.classList.remove("overlay-show");
+		}
+	}
+});
+
+form.addEventListener("submit", function (event) {
+	if (!name.value || !email.value) {
+		event.preventDefault();
+		popup.classList.add("modal-popup-error");
+	} else {
+		localStorage.setItem("name", name.value);
+	}
+});
